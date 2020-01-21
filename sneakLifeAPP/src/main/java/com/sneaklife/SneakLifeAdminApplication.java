bd34fb1527dece05abb04beb404ae46d9b7b6a5d
XiFYuW <xif_yuw@163.com>
2020-01-21 16:08:15 +0800

    new

--- sneakLifeAPP/src/main/java/com/sneaklife/SneakLifeAdminApplication.java
+++ sneakLifeAPP/src/main/java/com/sneaklife/SneakLifeAdminApplication.java
@@ -3,17 +3,19 @@ package com.sneaklife;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SneakLifeAdminApplication extends SpringBootServletInitializer {
+@ServletComponentScan
+public class SneakLifeAdminApplication extends SpringBootServletInitializer{
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakLifeAdminApplication.class, args);
+    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SneakLifeAdminApplication.class);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(SneakLifeAdminApplication.class, args);
     }
 }
